78250fa2421214492263881e923d0c63673043bb
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2022-02-26 16:30:30 +0700

    finish
    Co-authored-by: Noppawat Khamyot <noppa001@users.noreply.github.com>
    Co-authored-by: zentropy <pperorin@users.noreply.github.com>

--- HW_5/hw_5/src/component/membercard.js
+++ HW_5/hw_5/src/component/membercard.js
@@ -1,7 +1,9 @@
+import './style.css';
+
 function Membercard(props){
     return(
-      <div className = "card">
-        <div className="top">
+      <div className = "card-container">
+        <div className="card-detail">
           <div className="top-left">
             <h1 id="fontM">{props.firstname} {props.lastname}</h1>
             <h2 id="fontM">{props.id}</h2>
@@ -9,12 +11,16 @@ function Membercard(props){
               {/* {props.des} */}
             </div>
           </div>
-          <div className="top-right">
+          <div className="membercard-image">
             <img src={props.image} />
           </div>
         </div>
       </div>
     );
+}
+
+function Head(){
+  return;
 }
 
 export default Membercard
